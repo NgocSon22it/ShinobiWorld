11a8b1904f9436b25a0d9864b8c6061823d1cb94
NgocSon22it <102401522+NgocSon22it@users.noreply.github.com>
2023-03-31 13:13:26 +0700

    SetUp Menu

---
+++ ShinobiWorld/Assets/Scripts/Menu/Main/CustomCharacterManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomCharacterManager : MonoBehaviour
+{
+    [SerializeField] List<Toggle> ListToggle;
+
+    public void ToggleValueChanged(Toggle Toggle)
+    {
+        if (Toggle.isOn)
+        {
+            /*switch (ToggleValue) 
+            {
+                case "Melee":
+                    Debug.Log("Player selected Melee");
+                    break;
+                case "Range":
+                    Debug.Log("Player selected Range");
+                    break;
+                case "Support":
+                    Debug.Log("Player selected Support");
+                    break;
+                default:
+                    Debug.Log("Somethings Wrong!");
+                    break;
+            }*/
+
+
+        }
+    }
+
+}
