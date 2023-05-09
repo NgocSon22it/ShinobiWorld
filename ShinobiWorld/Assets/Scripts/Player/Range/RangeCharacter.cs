5074a610d7d3c126e7cf1c1193518cbe0a4fcd89
VoTanSang22 <votansangs2@gmail.com>
2023-05-09 15:08:15 +0700

    fixAnimationCharacter

--- ShinobiWorld/Assets/Scripts/Player/Range/RangeCharacter.cs
+++ ShinobiWorld/Assets/Scripts/Player/Range/RangeCharacter.cs
@@ -1,6 +1,8 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RangeCharacter : PlayerBase
 {
@@ -13,10 +15,76 @@ public class RangeCharacter : PlayerBase
     new void Update()
     {
         base.Update();
+        SkillOne();
+        SkillTwo();
+        SkillThree();
     }
 
     new void FixedUpdate()
     {
         base.FixedUpdate();
     }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            animator.SetTrigger("Attack_Range");
+        }
+    }
+
+    public void OnSkillOne(InputAction.CallbackContext context)
+    {
+        if (context.started && SkillOneCooldown_Current <= 0f)
+        {
+            PV.RPC(nameof(FindClostestEnemy), RpcTarget.AllBuffered);
+            SkillOneCooldown_Current = SkillOneCooldown_Total;
+            Debug.Log(Enemy.name);
+        }
+    }
+
+    public void OnSkillTwo(InputAction.CallbackContext context)
+    {
+        if (context.started && SkillTwoCooldown_Current <= 0f)
+        {
+            PV.RPC(nameof(FindClostestEnemy), RpcTarget.AllBuffered);
+            SkillTwoCooldown_Current = SkillTwoCooldown_Total;
+            Debug.Log(Enemy.name);
+        }
+    }
+
+    public void OnSkillThree(InputAction.CallbackContext context)
+    {
+        if (context.started && SkillThreeCooldown_Current <= 0f)
+        {
+            PV.RPC(nameof(FindClostestEnemy), RpcTarget.AllBuffered);
+            SkillThreeCooldown_Current = SkillThreeCooldown_Total;
+            Debug.Log(Enemy.name);
+        }
+    }
+
+    public void SkillOne()
+    {
+        if (SkillOneCooldown_Current > 0)
+        {
+            SkillOneCooldown_Current -= Time.deltaTime;
+        }
+    }
+
+    public void SkillTwo()
+    {
+        if (SkillTwoCooldown_Current > 0)
+        {
+            SkillTwoCooldown_Current -= Time.deltaTime;
+        }
+    }
+
+    public void SkillThree()
+    {
+        if (SkillThreeCooldown_Current > 0)
+        {
+            SkillThreeCooldown_Current -= Time.deltaTime;
+        }
+    }
+
 }
