a947306cdd09ff73388dac671961cb3e07839757
NgocSon22it <102401522+NgocSon22it@users.noreply.github.com>
2023-07-15 11:35:26 +0700

    Up Magician

--- ShinobiWorld/Assets/Scripts/Enemy/Normal/Locusts/Locusts.cs
+++ ShinobiWorld/Assets/Scripts/Enemy/Normal/Locusts/Locusts.cs
@@ -55,19 +55,7 @@ public class Locusts : Enemy
                 }
             }
         }
-        FindTarget_CurrentTime += Time.deltaTime;
-
-        // Check if the interval has passed
-        if (FindTarget_CurrentTime >= FindTarget_TotalTime)
-        {
-            TargetPosition = FindClostestTarget(detectionRadius, "Player");
-            FindTarget_CurrentTime = 0f;
-        }
-
-        playerInRange = TargetPosition != Vector3.zero;
-        // Restrict movement to the move area
-        clampedPosition = movementBounds.ClosestPoint(transform.position);
-        transform.position = new Vector3(clampedPosition.x, clampedPosition.y, transform.position.z);
+        
 
         animator.SetBool("PlayerInRange", playerInRange);
         animator.SetBool("Walk", isMoving);
